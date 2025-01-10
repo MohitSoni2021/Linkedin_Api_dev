ef5866caa9d5af117368195c8b7c091e6f493c7f
Mohit Soni <mohitsoni2006ms@gmail.com>
2025-01-10 15:02:17 +0530

    adding cors

--- linkedin_user_api.js
+++ linkedin_user_api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors())
 
 app.use(express.json());
 
